cc0d2735d2835c16232200b259acf1a48edcbf65
philbardawil <philbardawil@gmail.com>
2021-08-20 18:59:25 +0300

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Phil!'));
+app.get('/', (req, res) => res.send('Hello World and Phil!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
